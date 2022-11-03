12aade6a3159fd368e0f349e18c4b3077c963cdd
Grant Watson <gaw922@gmail.com>
2022-11-03 17:07:39 -0500

    UserDefaults for high score, reset rounds, and animations

--- MemoryPairs/ViewController.swift
+++ MemoryPairs/ViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class ViewController: UICollectionViewController {
-    var cards = ["🐶", "🐱", "🐭", "🐼", "🐻", "🐵", "🐨", "🦆", "🐰", "🐧"]
-    var pairs: [String]!
+    var cards = ["🐶", "🐱", "🐭", "🐼", "🐻", "🐵"]
+    var pairs = [String]()
     var selectedCards = [Card]()
     
     var highScore = 0
+    var tries = 0
     var score = 0 {
         didSet {
             navigationItem.rightBarButtonItem?.title = "Score: \(score)"
@@ -23,6 +24,8 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         
         title = "Pairs"
+        
+        // Make pairs of each of emojis to use in the game
         pairs = [[String]](repeating: cards, count: 2).flatMap{$0}.shuffled()
         
         let defaults = UserDefaults.standard
@@ -45,37 +48,49 @@ class ViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pairs.count
+        return cards.count * 2
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath) as! Card
         cell.label.isHidden = false
+        cell.label.alpha = 1
         selectedCards.append(cell)
         cell.isUserInteractionEnabled = false
         
         if selectedCards.count == 2 {
+            // If two selected cards match, gain a point and remove cards
             if selectedCards[0].label.text == selectedCards[1].label.text {
                 score += 1
-                self.pairs.removeAll { $0 == selectedCards[0].label.text! }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    collectionView.reloadData()
+                
+                displayMatch()
+                
+                // Remove cards from view
+                for card in selectedCards {
+                    card.isUserInteractionEnabled = true
+                    UIView.animate(withDuration: 0.75) {
+                        card.backgroundColor = UIColor.white
+                        card.label.alpha = 0
+                    }
                 }
                 
-                for card in self.selectedCards {
-                    card.isUserInteractionEnabled = true
-                }
+                pairs.removeAll(where: { $0 == selectedCards[0].label.text! })
+                
             } else {
-//                if score > 0 {
-//                    score -= 1
-//                }
-                for card in self.selectedCards {
+                // Give users 5 tries to memorize before deducting points
+                tries += 1
+                if tries > 5 {
+                    score -= 1
+                }
+                // "Flip" cards back to original state
+                for card in selectedCards {
                     card.isUserInteractionEnabled = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        card.label.isHidden = true
+                    UIView.animate(withDuration: 0.75) {
+                        card.label.alpha = 0
                     }
                 }
             }
+            
             selectedCards.removeAll()
         }
         
@@ -95,16 +110,18 @@ class ViewController: UICollectionViewController {
     @objc func resetGame() {
         pairs = [[String]](repeating: cards, count: 2).flatMap{$0}.shuffled()
         score = 0
+        tries = 0
         collectionView.reloadData()
     }
     
     func saveHighScore() {
         let defaults = UserDefaults.standard
+        // Retrieve high score, if not one, set it one
         guard let savedScore = defaults.value(forKey: "highScore") as? Int else {
             defaults.set(score, forKey: "highScore")
             return
         }
-        
+        // Set new high score if score is higher than saved score.
         if score > savedScore {
             defaults.set(score, forKey: "highScore")
             highScore = score
@@ -115,6 +132,19 @@ class ViewController: UICollectionViewController {
         let ac = UIAlertController(title: "High Score", message: "\(highScore)", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    
+    func displayMatch() {
+        let match = UIImageView(image: UIImage(named: "match"))
+        match.frame = CGRect(x: (view.frame.width / 2) - 175, y: (view.frame.height / 2) - 50, width: 350, height: 100)
+        match.alpha = 0
+        view.addSubview(match)
+        UIView.animate(withDuration: 1.75, animations: {
+            match.alpha = 1
+            match.alpha = 0
+        }) { _ in
+            match.removeFromSuperview()
+        }
     }
 }
 
